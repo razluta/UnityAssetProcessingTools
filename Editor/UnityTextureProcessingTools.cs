a3f9b6fd160651618aea0b27c3a120c9ff9aa2ef
Razvan Luta <razvan.luta@gmail.com>
2020-07-06 17:30:46 -0700

    Added a few more segments for the Filtering Tab GUI.

--- Editor/UnityTextureProcessingTools.cs
+++ Editor/UnityTextureProcessingTools.cs
@@ -1,43 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using UnityEditor;
-using UnityEditor.UIElements;
+﻿using UnityEditor;
 using UnityEngine;
-using UnityEngine.Profiling;
-using UnityEngine.U2D;
 using UnityEngine.UIElements;
-using Object = UnityEngine.Object;
 
-public class UnityTextureProcessingTools : EditorWindow
+namespace UnityTextureProcessingTools.Editor
 {
-    private VisualElement _root;
-
-    [MenuItem("Art Tools/Texture Processing Tools")]
-    public static void ShowWindow()
-    {
-        // Opens the window, otherwise focuses it if it’s already open.
-        var window = GetWindow<UnityTextureProcessingTools>();
-
-        // Adds a title to the window.
-        window.titleContent = new GUIContent("Texture Processing Tools");
-
-        // Sets a minimum size to the window.
-        window.minSize = new Vector2(350, 550);
-    }
-    
-    private void OnEnable()
-    {
-        // Reference to the root of the window.
-        _root = rootVisualElement;
-        
-        // Associates a stylesheet to our root. Thanks to inheritance, all root’s
-        // children will have access to it.
-        _root.styleSheets.Add(Resources.Load<StyleSheet>("UnityTextureProcessingToolsEditorWindow"));
-
-        // Loads and clones our VisualTree (eg. our UXML structure) inside the root.
-        var mainVisualTree = Resources.Load<VisualTreeAsset>("UnityTextureProcessingToolsEditorWindow");
-        mainVisualTree.CloneTree(_root);
-    }
+    public class UnityTextureProcessingTools : EditorWindow                                                                                  
+    {                                                                                                                                        
+        private VisualElement _root;                                                                                                         
+                                                                                                                                             
+        [MenuItem("Art Tools/Texture Processing Tools")]                                                                                     
+        public static void ShowWindow()                                                                                                      
+        {                                                                                                                                    
+            // Opens the window, otherwise focuses it if it’s already open.                                                                  
+            var window = GetWindow<UnityTextureProcessingTools>();                                                                           
+                                                                                                                                             
+            // Adds a title to the window.                                                                                                   
+            window.titleContent = new GUIContent("Texture Processing Tools");                                                                
+                                                                                                                                             
+            // Sets a minimum size to the window.                                                                                            
+            window.minSize = new Vector2(350, 600);                                                                                          
+        }                                                                                                                                    
+                                                                                                                                             
+        private void OnEnable()                                                                                                              
+        {                                                                                                                                    
+            // Reference to the root of the window.                                                                                          
+            _root = rootVisualElement;                                                                                                       
+                                                                                                                                             
+            // Associates a stylesheet to our root. Thanks to inheritance, all root’s                                                        
+            // children will have access to it.                                                                                              
+            _root.styleSheets.Add(Resources.Load<StyleSheet>(
+                "UnityTextureProcessingToolsEditorWindow"));                                    
+                                                                                                                                             
+            // Loads and clones our VisualTree (eg. our UXML structure) inside the root.                                                     
+            var mainVisualTree = Resources.Load<VisualTreeAsset>(
+                "UnityTextureProcessingToolsEditorWindow");                                 
+            mainVisualTree.CloneTree(_root);                                                                                                 
+        }                                                                                                                                    
+    }                                                                                                                                        
 }
