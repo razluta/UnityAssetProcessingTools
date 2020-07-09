401395ec6d100ce78cb45e087e89ea4537f261d5
Razvan Luta <razvan.luta@gmail.com>
2020-07-09 01:35:32 -0700

    Started to dynamically create the UI.

--- Editor/UnityAssetProcessingTools.cs
+++ Editor/UnityAssetProcessingTools.cs
@@ -1,5 +1,7 @@
 ﻿using System.Linq;
+using System.Reflection;
 using UnityEditor;
+using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -7,16 +9,23 @@ namespace UnityAssetProcessingTools.Editor
 {
     public class UnityAssetProcessingTools : EditorWindow                                                                                  
     {                                                                                                                                        
-        private VisualElement _root;   
+        private VisualElement _root;
+        private VisualTreeAsset _mainVisualTree;
 
+        private VisualTreeAsset _activeFilterVisualTreeAsset;
+        private VisualElement _toolsTabsVisualElement;
+        private VisualTreeAsset _tabButtonVisualTreeAsset;
         private TemplateContainer _filterTabTemplateContainer;
         private TemplateContainer _renamingTabTemplateContainer;
-        private TemplateContainer _resizingTabTemplateContainer;
+        private Button _filterTabButton;
+        private Button _renamingTabButton;
+        private VisualElement _assetTypesVisualElement;
+        private Button _allAssetTypeTabButton;
+        private VisualTreeAsset _filterAllAssetsVisualTreeAsset;
+        private VisualElement _filterAllAssetsTabContentsVisualElement;
+        private VisualTreeAsset _versionInfoVisualTreeAsset;
+        private VisualElement _versionInfoVisualElement;
 
-        private VisualElement _filterTabVisualElement;
-        private VisualElement _renamingTabVisualElement;
-        private VisualElement _resizingTabVisualElement;
-                                                                                                                                             
         [MenuItem("Art Tools/Launch Asset Processing Tools")]                                                                                     
         public static void ShowWindow()                                                                                                      
         {                                                                                                                                    
@@ -40,27 +49,78 @@ namespace UnityAssetProcessingTools.Editor
             _root.styleSheets.Add(Resources.Load<StyleSheet>(
                 "UnityAssetProcessingToolsEditorWindow"));                                    
                                                                                                                                              
-            // Loads and clones our VisualTree (eg. our UXML structure) inside the root.                                                     
-            var mainVisualTree = Resources.Load<VisualTreeAsset>(
-                "UnityAssetProcessingToolsEditorWindow");                                 
-            mainVisualTree.CloneTree(_root);
-            
-            // Get references to the tab buttons
-            _filterTabTemplateContainer = _root.Q<TemplateContainer>("BT_FilterTab");
-            var filterTabButton = (Button) _filterTabTemplateContainer.Children().ToArray()[0];
-            filterTabButton.text = "Filter";
-            filterTabButton.clickable.clicked += () => ShowFilterTab();
-            
-            _renamingTabTemplateContainer = _root.Q<TemplateContainer>("BT_RenamingTab");
-            var renamingTabButton = (Button) _renamingTabTemplateContainer.Children().ToArray()[0];
-            renamingTabButton.text = "Renaming";
-            renamingTabButton.clickable.clicked += () => ShowFilterTab();
+            // // Loads and clones our VisualTree (eg. our UXML structure) inside the root.                                                     
+            // _mainVisualTree = Resources.Load<VisualTreeAsset>(
+            //     "UnityAssetProcessingToolsEditorWindow");                                 
+            // _mainVisualTree.CloneTree(_root);
 
-            // Get references to the tab contents
-            _filterTabVisualElement = _root.Q<VisualElement>("VE_FilterTab");
+            // Initiate the GUI
+            InitUi();
 
             // Initiate default state
             ShowFilterTab();
+        }
+
+        private void InitUi()
+        {
+            // Active filter
+            _activeFilterVisualTreeAsset = Resources.Load<VisualTreeAsset>("CS_FilterActive");
+            _activeFilterVisualTreeAsset.CloneTree(_root);
+            
+            // Tools tabs
+            _toolsTabsVisualElement = new VisualElement();
+            _root.Add(_toolsTabsVisualElement);
+            _toolsTabsVisualElement.style.flexDirection = FlexDirection.Row;
+            _toolsTabsVisualElement.style.flexShrink = 0;
+            _toolsTabsVisualElement.style.flexGrow = 0;
+            _toolsTabsVisualElement.style.marginBottom = 8;
+            _toolsTabsVisualElement.style.marginTop = 8;
+            _toolsTabsVisualElement.style.alignItems = Align.Center;
+            _toolsTabsVisualElement.style.justifyContent = Justify.Center;
+            
+            _tabButtonVisualTreeAsset = Resources.Load<VisualTreeAsset>("BT_Tab");
+            
+            _tabButtonVisualTreeAsset.CloneTree(_toolsTabsVisualElement);
+            _filterTabButton = _root.Q<Button>("BT_Tab");
+            _filterTabButton.name = "BT_FilterTab";
+            _filterTabButton.text = "Filter";
+            _filterTabButton.clickable.clicked += () => ShowFilterTab();
+            
+            _tabButtonVisualTreeAsset.CloneTree(_toolsTabsVisualElement);
+            _renamingTabButton = _root.Q<Button>("BT_Tab");
+            _renamingTabButton.name = "BT_RenamingTab";
+            _renamingTabButton.text = "Renaming";
+            _renamingTabButton.clickable.clicked += () => ShowRenamingTab();
+            
+            // Asset Type Tab
+            _assetTypesVisualElement = new VisualElement();
+            _root.Add(_assetTypesVisualElement);
+            _assetTypesVisualElement.style.flexDirection = FlexDirection.Row;
+            _assetTypesVisualElement.style.flexShrink = 0;
+            _assetTypesVisualElement.style.flexGrow = 0;
+            _assetTypesVisualElement.style.marginBottom = 8;
+            _assetTypesVisualElement.style.marginTop = 8;
+            _assetTypesVisualElement.style.alignItems = Align.Center;
+            _assetTypesVisualElement.style.justifyContent = Justify.Center;
+            
+            _tabButtonVisualTreeAsset.CloneTree(_assetTypesVisualElement);
+            _allAssetTypeTabButton = _filterTabButton = _root.Q<Button>("BT_Tab");
+            _allAssetTypeTabButton.name = "BT_AllAssetTypes";
+            _allAssetTypeTabButton.text = "All Asset Types";
+            
+            // Filter All Assets Tab Contents
+            _filterAllAssetsVisualTreeAsset = Resources.Load<VisualTreeAsset>("CS_FilterAllAssetsTab");
+            _filterAllAssetsVisualTreeAsset.CloneTree(_root);
+            _filterAllAssetsTabContentsVisualElement = _root.Q<VisualElement>("VE_FilterAllAssetsTabContents");
+            _filterAllAssetsTabContentsVisualElement.style.flexShrink = 0;
+            _filterAllAssetsTabContentsVisualElement.style.flexGrow = 1;
+            
+            // Version info
+            _versionInfoVisualTreeAsset = Resources.Load<VisualTreeAsset>("CS_Version");
+            _versionInfoVisualTreeAsset.CloneTree(_root);
+            _versionInfoVisualElement = _root.Q<VisualElement>("CS_Version");
+            _versionInfoVisualElement.style.flexShrink = 0;
+            _versionInfoVisualElement.style.flexGrow = 0;
         }
 
         private void HideAllTabs()
@@ -69,6 +129,11 @@ namespace UnityAssetProcessingTools.Editor
         }
         
         private void ShowFilterTab()
+        {
+            HideAllTabs();
+        }
+        
+        private void ShowRenamingTab()
         {
             HideAllTabs();
         }
