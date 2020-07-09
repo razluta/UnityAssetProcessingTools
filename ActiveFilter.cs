3394c95666b24ede08c43ad82a17977ef7888838
Razvan Luta <razvan.luta@gmail.com>
2020-07-09 02:01:48 -0700

    Added an Active Filter class.

---
+++ ActiveFilter.cs
@@ -0,0 +1,15 @@
+﻿namespace UnityAssetProcessingTools
+{
+    public class ActiveFilter
+    {
+        private string AssetDirectoryPath { get; set; }
+        private bool IsDirectorySearchRecursive { get; set; }
+        private string NameStartsWith  { get; set; }
+        private string NameContains  { get; set; }
+        private string NameEndsWith  { get; set; }
+        private int DiskSize  { get; set; }
+    }
+}
+
+
+
