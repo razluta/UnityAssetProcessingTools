c15109da7872e0f056931de50b800b7498b638a0
Razvan Luta <razvan.luta@gmail.com>
2020-07-09 02:02:25 -0700

    Added a JsonUtilitiesClass.

---
+++ SystemUtilities/JsonUtilities.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace UnityAssetProcessingTools.SystemUtilities
+{
+    public class JsonUtilities
+    {
+        public void SerializeJsonDictionaryToPath(Dictionary<string, string> data, string path)
+        {
+            
+        }
+    }
+}
